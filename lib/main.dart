c278531ce52fcacc1e556957445507de7bd518d4
lokeshbs1994 <lokeshbs619@gmail.com>
2022-04-28 21:08:07 +0530

    deeplinking

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text('Deep Link')),
+        body: Center(
+            child: Container(
+          child: Text("DeepLinking Page"),
+        )),
+      ),
+    );
+  }
+}
